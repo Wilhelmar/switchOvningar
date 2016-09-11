22bd8106813365270a9b80bfffdddd21cd525d70
wilhelmar <wilhelmar@vgy.se>
2016-09-11 10:53:42 +0200

    gjorde övning 1 och 2

--- switchsatser/Program.cs
+++ switchsatser/Program.cs
@@ -10,6 +10,54 @@ namespace switchsatser
     {
         static void Main(string[] args)
         {
+            /*
+
+             //Övning 1
+
+             Console.Write("Mata in ett tal: ");
+             int tal = int.Parse(Console.ReadLine());
+
+             switch (tal)
+             {
+                 case 1:
+                     Console.WriteLine("Ett");
+                     break;
+                 case 2:
+                     Console.WriteLine("Två");
+                     break;
+                 case 3:
+                     Console.WriteLine("Tre");
+                     break;
+                 case 4:
+                     Console.WriteLine("Fyra");
+                     break;
+                 default:
+                     Console.WriteLine("Ogiltig inmatning!!");
+                     break;
+             }
+
+    
+
+            //Övning 2
+
+
+            Console.WriteLine("Kolla åt vilket håll du är på väg!");
+            Console.Write("Mata in ett väderstreck: ");
+            char heading = char.Parse(Console.ReadLine());
+
+            switch (heading)
+            {
+                case 'N': Console.WriteLine("Du är på väg norrut!");                     break;
+                case 'E': Console.WriteLine("Du är på väg österut!");                    break;
+                case 'W': Console.WriteLine("Du är på väg västerut!");                   break;
+                case 'S': Console.WriteLine("Du är på väg söderut!");                    break;
+                default: Console.WriteLine("Okänd inmatning! Du är på väg mot Mars???"); break;
+                    
+            }
+
+            
+ */
+
         }
     }
 }
